69a823e14d077998068f0356f267c96f88c92d76
MeRaj-Rudba <mrudba@gmail.com>
2022-02-24 13:56:56 +0600

    offline support done

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
-import React, { useEffect, useState } from "react";
-import Screen from "./app/components/Screen";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import { Button, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AuthNavigator from "./app/navigation/AuthNavigator";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
-
-//local imports
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -30,12 +30,14 @@ function ListingsScreen({ navigation }) {
       <FlatList
         data={getListingsApi.data}
         keyExtractor={(listing) => listing.id.toString()}
+        showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
           <Card
             title={item.title}
             subTitle={"$" + item.price}
             imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+            thumbnailUrl={item.images[0].thumbnailUrl}
           />
         )}
       />
